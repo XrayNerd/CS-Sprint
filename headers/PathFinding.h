2cab0fea9d1b43ed93a890a7f9d365c3e50c0e99
Stark98f <andufabian@icloud.com>
2018-02-10 01:12:50 +0000

    Initial Release

--- headers/PathFinding.h
+++ headers/PathFinding.h
@@ -1,9 +1,42 @@
-include <vector>
+#ifndef PATHFINDING_H
+#define PATHFINDING_H
+
+#include <vector>
+#include "global.h"
 
 class PathFinding
 {
 public:
   PathFinding(void);
   ~PathFinding(void);
-  void FindPath();
-}
+
+  void FindPath(Vector2 currentPos, Vector2 targetPos);
+  Vector2 NextPathPos();
+
+  void ClearOpenList()
+  {m_openList.clear();}
+
+  void ClearVisitedList()
+  {n_visitedList.clear() ;}
+
+  void ClearPathToObj()
+  {m_pathToObj.clear();}
+
+  bool m_initializedStartObj ;
+  bool m_foundObj ;
+
+private:
+
+  void SetStartAndObj (SearchCell start, SearchCell obj);
+  void PathOpened (int x , int z, float newCost, SearchCell *parent );
+  SearchCell *GetNextCell();
+  void ContinuePath();
+
+  SearchCell *m_startCell;
+  SearchCell *m_goalCell;
+  std::vector<SearchCell*> m_openList;
+  std::vector<SearchCell*> m_visitedList;
+  std::vector<Vector2*>m_pathToObj;
+};
+
+#endif
--- headers/SearchTile.h
+++ headers/SearchTile.h
@@ -1,26 +1,26 @@
-/*  SearchTile
-*   Search tile finds the shortest path between two nodes.
+/*  SearchCell
+*   Search Cell finds the shortest path between two nodes.
 */
 
 #include <math.h>
 
 #define WORLD_SIZE
 
-struct SearchTile
+struct SearchCell
 {
 public:
     int n_xcoord, n_ycoord;
     int n_id;
-    SearchTile *parent;
+    SearchCell *parent;
     float G; //Movement cost from the start point to current square
     float H; //Estimated movement cost from current square to the destination point
 
-    SearchTile() : parent(0) {}
-    SearchTile(int x ,int y, SearchTile *_parent=0) : n_xcoord, n_ycoord, parent(_parent),
+    SearchCell() : parent(0) {}
+    SearchCell(int x ,int y, SearchCell *_parent=0) : n_xcoord, n_ycoord, parent(_parent),
      n_id(y * WORLD_SIZE + x), G(0), H(0) {};
 
      float GetF() {return G+H;}
-     float ManHattanDistance(SearchTile *nodeEnd)
+     float ManHattanDistance(SearchCell *nodeEnd)
      {
        float x = (float)(fabs(this->n_xcoord - nodeEnd -> n_xcoord));
        float y = (float)(fabs(this->n_ycoord - nodeEnd -> n_ycoord));
